67494870e1a17f7881996af4e8a035e8eaaee463
Ryan McMinds <r.mcminds@gmail.com>
2021-09-22 10:20:07 -0400

    Update pipeline_sketch.sh

--- microbiomes/telomeres/pipeline_sketch.sh
+++ microbiomes/telomeres/pipeline_sketch.sh
@@ -1,23 +1,26 @@
+## define variables
 source local.sh
-
-module purge 
-module load apps/sratoolkit/2.10.7
 
 ## download sequence data (below is command for single sample)
 ## https://www.ncbi.nlm.nih.gov/sra/docs/sra-aws-download/
-fasterq-dump --split-3 SRR6793695 -O ${output_dir}/SRR6793695
+module purge 
+module load apps/sratoolkit/2.10.7
+fasterq-dump --split-3 ${sample} -O ${output_dir}/${sample}
 
+## assemble genome. IRL this should be done with concatenated set of all samples from study, not once-per-sample. Otherwise of course use a reference genome if available
 module purge 
 module load apps/spades/3.11.1
-spades.py --pe1-1 ${output_dir}/SRR6793695/SRR6793695_1.fastq --pe1-2 ${output_dir}/SRR6793695/SRR6793695_2.fastq -o ${output_dir}/SRR6793695/spades
+spades.py --pe1-1 ${output_dir}/${sample}/${sample}_1.fastq --pe1-2 ${output_dir}/${sample}/${sample}_2.fastq -o ${output_dir}/${sample}/spades
 
+## align to genome
 module purge 
 module load hub.apps/anaconda3
 conda activate bowtie2
 module load apps/samtools/1.3.1
-bowtie2-build ${output_dir}/SRR6793695/spades/scaffolds.fasta ${output_dir}/SRR6793695/spades/genome
-bowtie2 -x ${output_dir}/SRR6793695/spades/genome -1 ${output_dir}/SRR6793695/SRR6793695_1.fastq -2 ${output_dir}/SRR6793695/SRR6793695_2.fastq | samtools view -bS - > ${output_dir}/SRR6793695/aligned_reads.bam
+bowtie2-build ${output_dir}/${sample}/spades/scaffolds.fasta ${output_dir}/${sample}/spades/genome
+bowtie2 -x ${output_dir}/${sample}/spades/genome -1 ${output_dir}/${sample}/${sample}5_1.fastq -2 ${output_dir}/${sample}/${sample}5_2.fastq | samtools view -bS - > ${output_dir}/${sample}/aligned_reads.bam
 
+## run telomerecat
 conda deactivate
 conda activate telomerecat
-telomerecat bam2length --output ${output_dir}/SRR6793695.csv ${output_dir}/SRR6793695/aligned_reads.bam
+telomerecat bam2length --output ${output_dir}/${sample}.csv ${output_dir}/${sample}/aligned_reads.bam
