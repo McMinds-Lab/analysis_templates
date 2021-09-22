cc15b9598daa72220f759026d46a131582abff03
Ryan McMinds <r.mcminds@gmail.com>
2021-09-22 17:08:40 -0400

    Update pipeline_sketch.sh

--- microbiomes/telomeres/pipeline_sketch.sh
+++ microbiomes/telomeres/pipeline_sketch.sh
@@ -14,6 +14,8 @@ conda activate picard
 picard FastqToSam -F1 ${output_dir}/${sample}/${sample}_1.fastq -F2 ${output_dir}/${sample}/${sample}_2.fastq -O ${output_dir}/${sample}/unaligned_read_pairs.bam -SM ${sample} -SO unsorted
 
 ## telomerecat requires a specific header format for some reason
+conda deactivate
+module purge 
 module load apps/samtools/1.3.1
 samtools view -H ${output_dir}/${sample}/unaligned_read_pairs.bam > ${output_dir}/${sample}/myheader.sam
 printf "@SQ\tSN:fake_contig\tLN:1\n" >> ${output_dir}/${sample}/myheader.sam 
@@ -21,6 +23,7 @@ samtools reheader ${output_dir}/${sample}/myheader.sam ${output_dir}/${sample}/u
 mv ${output_dir}/${sample}/unaligned_read_pairs.bam_tmp ${output_dir}/${sample}/unaligned_read_pairs.bam
 
 ## run telomerecat
-conda deactivate
+module purge 
+module load hub.apps/anaconda3
 conda activate telomerecat
 telomerecat bam2length --output ${output_dir}/${sample}.csv ${output_dir}/${sample}/unaligned_read_pairs.bam
