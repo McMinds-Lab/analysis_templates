507b4278131d81891de055882916ea5a448dc344
Shane Alexander [salexander4] <salexander4@rra-login0.rc.usf.edu>
2021-10-30 23:10:59 -0400

    Small modifications

--- microbiomes/qc_16S/1_init_QC.sh
+++ microbiomes/qc_16S/1_init_QC.sh
@@ -18,8 +18,14 @@ for file in ${indir}/*_R1*fastq.gz; do
 	## trim primers from sequences, and discard any sequences that don't contain both full primers exactly at the beginnings of the reads.
     # (a) merge paired-end reads such that short reads, where the read is longer than the insertion (such as mitochondria), are not discarded, and nucleotides are trimmed that extend past the beginning of the paired read (which are just adaptor sequences)
     ## (b) filter merged reads based on 'expected errors' and a minimum read length. This function determines the likelihood that each read has an error based on all the (merged) quality scores. If even one error is expected to occur within the read (including even the presence of a single 'N'), it throws out the entire read. No trimming occurs, which is theoretically better for downstream clustering and denoising applications. This command also changes the fasta headers in the output so they fit the format ">sampleID_arbitraryreadnumber"
-	cutadapt --discard-untrimmed --pair-filter=any -g "^${fwdprimer}" -G "^${revprimer}" -o ${outdir}/trimmed/${sampleid}_R1.fastq -p ${outdir}/trimmed/${sampleid}_R2.fastq ${file} ${file/R1/R2}
 
+module purge
+module load hub.apps/anaconda3
+source activate cutadapt
+	cutadapt --discard-untrimmed --pair-filter=any -g "^${fwdprimer}" -G "^${revprimer}" -o ${outdir}/trimmed/${sampleid}_R1.fastq -p ${outdir}/trimmed/${sampleid}_R2.fastq ${file} ${file/R1/R2}
+conda deactivate
+module purge
+module load apps/vsearch
 	vsearch --fastq_mergepairs ${outdir}/trimmed/${sampleid}_R1.fastq --reverse ${outdir}/trimmed/${sampleid}_R2.fastq --fastq_allowmergestagger --fasta_width 0 --threads ${nthreads} --fastqout - |
 
 	vsearch --fastq_filter - --fastq_maxns 1 --fastq_maxee 1 --fastq_ascii 33 --fasta_width 0 --relabel ${sampleid}_ --threads ${nthreads} --fastaout ${outdir}/filtered/${sampleid}.fasta
