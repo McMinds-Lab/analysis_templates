02576d9f6f5fe0b08906490937c7c0be7727fe0c
salexander4 <salexander4@rra-login1.rc.usf.edu>
2021-11-10 14:18:43 -0500

    customized to data

--- microbiomes/qc_16S/1_init_QC.sh
+++ microbiomes/qc_16S/1_init_QC.sh
@@ -1,15 +1,26 @@
-source ~/.bashrc
+#!/bin/bash
+#SBATCH --job-name=1_init-qc
+#SBATCH --partition=rra
+#SBATCH --qos=rra
+#SBATCH --mail-user=salexander4@usf.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=logs/1_init-qc.out
+#SBATCH --error=logs/1_init-qc.err
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20#SBATCH --time=01:00:00
 
-outdir=/raid1/home/micro/mcmindsr/ryan/20170228_swarm_plus_MED2/
-indir=/raid1/home/micro/mcmindsr/labhome/web_downloads/GCMP/data/16S/raw_data/fastqs/
+outdir=$WORK/output/dada2_tutorial
+indir=$WORK/input/dada2_tutorial/MiSeq_SOP
 nthreads=1
-fwdprimer=GTGYCAGCMGCCGCGGTAA
-revprimer=GGACTACNVGGGTWTCTAAT
+#fwdprimer=GTGYCAGCMGCCGCGGTAA
+#revprimer=GGACTACNVGGGTWTCTAAT
 
 mkdir -p ${outdir}/trimmed/
 mkdir -p ${outdir}/filtered/
 
-for file in ${indir}/*_R1*fastq.gz; do
+for file in ${indir}/*_R1*fastq; do
 
     # trim "R1" from filenames to get Sample IDs that match mapping file
     filename=$(basename $file)
@@ -22,7 +33,7 @@ for file in ${indir}/*_R1*fastq.gz; do
 module purge
 module load hub.apps/anaconda3
 source activate cutadapt
-	cutadapt --discard-untrimmed --pair-filter=any -g "^${fwdprimer}" -G "^${revprimer}" -o ${outdir}/trimmed/${sampleid}_R1.fastq -p ${outdir}/trimmed/${sampleid}_R2.fastq ${file} ${file/R1/R2}
+	cutadapt --discard-untrimmed  --pair-filter=any -o ${outdir}/trimmed/${sampleid}_R1.fastq -p ${outdir}/trimmed/${sampleid}_R2.fastq ${file} ${file/R1/R2}
 conda deactivate
 module purge
 module load apps/vsearch
@@ -36,4 +47,5 @@ done
 
 cat ${outdir}/filtered/*.fasta > ${outdir}/seqs.fna
 
-rm -r ${outdir}/trimmed/ ${outdir}/filtered/
+#rm -r ${outdir}/trimmed/ ${outdir}/filtered/
+
