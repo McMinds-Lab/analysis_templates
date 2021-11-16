5611fd3c391ebaef19c6680088ab499b1048c488
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2021-11-15 22:09:00 -0500

    bugfix

--- microbiomes/qc_16S/01_init_QC.sh
+++ microbiomes/qc_16S/01_init_QC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=01_init_qc
+#SBATCH --job-name=01_init_QC
 #SBATCH --partition=rra
 #SBATCH --qos=rra
 #SBATCH --mail-user=salexander4@usf.edu
 #SBATCH --mail-type=END,FAIL
-#SBATCH --output=logs/01_init_qc.out
+#SBATCH --output=logs/01_init_QC.out
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
@@ -18,8 +18,8 @@ barcodes_fwd=barcodes_fwd.fasta
 barcodes_rev=barcodes_rev.fasta
 nthreads=1
 
-mkdir -p ${outdir}/1_init_QC/demultiplexed/
-mkdir -p ${outdir}/1_init_QC/merged
+mkdir -p ${outdir}/01_init_QC/demultiplexed/
+mkdir -p ${outdir}/01_init_QC/merged
 
 # trim indices and primers from sequences, demultiplex, and discard any sequences that don't contain both full barcodes and primers
 module purge
@@ -31,12 +31,12 @@ cutadapt \
   --pair-filter=any \
   -g file:${barcodes_fwd} \
   -G file:${barcodes_rev} \
-  -o ${outdir}/trimmed/{name1}-{name2}_R1.fastq \
-  -p ${outdir}/trimmed/{name1}-{name2}_R2.fastq \
+  -o ${outdir}/01_init_QC/demultiplexed/{name1}-{name2}_R1.fastq \
+  -p ${outdir}/01_init_QC/demultiplexed/{name1}-{name2}_R2.fastq \
   ${in_fwd} \
   ${in_rev}
 
-for file in ${outdir}/1_init_QC/demultiplexed/*_R1.fastq; do
+for file in ${outdir}/01_init_QC/demultiplexed/*_R1.fastq; do
 
   # trim "R1" from filenames to get Sample IDs that match mapping file
   filename=$(basename $file)
@@ -53,7 +53,7 @@ for file in ${outdir}/1_init_QC/demultiplexed/*_R1.fastq; do
     --fastq_allowmergestagger \
     --fasta_width 0 \
     --threads ${nthreads} \
-    --fastqout ${outdir}/1_init_QC/merged/${sampleid}.fastq
+    --fastqout ${outdir}/01_init_QC/merged/${sampleid}.fastq
 
 done
 
