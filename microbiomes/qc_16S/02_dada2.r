0037bd52b94ffd0065f44b7981579d7f6f85d46f
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2021-11-15 23:04:42 -0500

    dada2 template

--- microbiomes/qc_16S/02_dada2.r
+++ microbiomes/qc_16S/02_dada2.r
@@ -1,12 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=02_dada2
-#SBATCH --partition=rra
-#SBATCH --qos=rra
-#SBATCH --mail-user=salexander4@usf.edu
-#SBATCH --mail-type=END,FAIL
-#SBATCH --output=logs/02_dada2.out
-#SBATCH --ntasks=1
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=20
-#SBATCH --time=01:00:00
+library(dada2)
+
+#do dada2 stuff
