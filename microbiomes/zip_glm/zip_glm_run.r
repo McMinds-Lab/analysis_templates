88a9d4405ff4695c1923136743c2d82203a8b044
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2022-01-24 13:40:14 -0500

    use old silva because euks missing from latest

--- microbiomes/zip_glm/zip_glm_run.r
+++ microbiomes/zip_glm/zip_glm_run.r
@@ -16,6 +16,7 @@ opencl <- as.logical(newargs[[11]])
 opencl_device <- as.numeric(newargs[[12]])
 model_dir <- newargs[[13]]
 algorithm <- newargs[[14]]
+taxalg <- newargs[[15]]
 ##
 
 rownames(seqtab) <- sapply(rownames(seqtab), function(x) samplenames$Sample[samplenames$Tag == sub('.fastq.gz','',sub('-',':',x))])
@@ -28,17 +29,23 @@ metadat$id_argaly <- id_conversion$id_argaly[match(metadat$ID..Alison, id_conver
 
 m2 <- metadat[match(rownames(seqtab_M),metadat$id_argaly),]
 
-dna <- Biostrings::DNAStringSet(dada2::getSequences(seqtab))
-ids <- DECIPHER::IdTaxa(dna, trainingSet, strand="top", processors=NULL, verbose=FALSE) 
-ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species") 
-# Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
-taxid <- t(sapply(ids, function(x) {
-  m <- match(ranks, x$rank)
-  taxa <- x$taxon[m]
-  taxa[startsWith(taxa, "unclassified_")] <- NA
-  taxa
-}))
-colnames(taxid) <- ranks; rownames(taxid) <- dada2::getSequences(seqtab)
+if(taxalg == 'decipher') {
+  dna <- Biostrings::DNAStringSet(dada2::getSequences(seqtab))
+  ids <- DECIPHER::IdTaxa(dna, trainingSet, strand="top", processors=NULL, verbose=FALSE) 
+  ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species") 
+  # Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
+  taxid <- t(sapply(ids, function(x) {
+    m <- match(ranks, x$rank)
+    taxa <- x$taxon[m]
+    taxa[startsWith(taxa, "unclassified_")] <- NA
+    taxa
+  }))
+  colnames(taxid) <- ranks; rownames(taxid) <- dada2::getSequences(seqtab)
+} else {
+  ## https://zenodo.org/record/1172783#.Ye7xEvXMLxg
+  taxid <- dada2::assignTaxonomy(seqtab, "~/data/ref/silva_nr_v132_train_set.fa.gz", multithread=nthreads)
+  taxid <- dada2::addSpecies(taxid, '~/data/ref/silva_species_assignment_v132.fa.gz', allowMultiple=TRUE)
+}
 
 ## set up output directory
 dir.create(file.path(outdir, 'zip_glm'))
@@ -148,12 +155,12 @@ sampling_commands <- list(hmc = paste('./zip_glm',
                                       'algorithm=hmc',
                                       'stepsize=0.1',
                                       'engine=nuts',
-                                      'max_depth=10',
+                                      'max_depth=12',
                                       'adapt t0=10',
                                       'delta=0.8',
                                       'kappa=0.75',
-                                      'num_warmup=200',
-                                      'num_samples=200',
+                                      'num_warmup=1000',
+                                      'num_samples=1000',
                                       paste0('num_threads=',nthreads),
                                       (paste0('opencl platform=0 device=', opencl_device))[opencl],
                                       sep=' '),
