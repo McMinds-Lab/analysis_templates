ab6266a2f0a4a785c92924ca7ecb97f25a4c90ae
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2022-02-01 11:19:04 -0500

    add qr

---
+++ microbiomes/zip_glm/zip_glm_qr.stan
@@ -0,0 +1,125 @@
+functions {
+    int num_zeros(int[] y) {
+      int nz = 0;
+      for (n in 1:size(y))
+          nz += (y[n] == 0);
+      return nz;
+    }
+    real zip_lpmf(int[] c, vector p, vector a, int[] i0, int[] in0) {
+        vector[size(p)] lilp = log_inv_logit(p);
+        real lp = sum(lilp[in0]) + poisson_log_lpmf(c[in0] | a[in0]);
+        for(i in i0) {
+            lp += log_sum_exp(log1m_inv_logit(p[i]), lilp[i] - exp(a[i])); // poisson lpmf with count 0 = log(exp(-exp(log_lambda)))
+        }
+        return(lp);
+    }
+}
+data {
+    int NS;                                 // number of samples
+    int NB_s;                               // number of sample factor levels
+    int NSB;                                // number of sample factors
+    array[NB_s] int idx_s;                  // mapping of sigmas to sample factor levels
+    matrix[NS,NB_s] X_s;                    // model matrix for samples (e.g. tissue compartments, duplicates, sequencing depth, etc.). must include intercept but not residuals
+    matrix[NB_s-1,NB_s-1] X_sR_inv;         // inverse(qr_thin_R(X_s[,2:])), precomputed in R to use pivoting and be more stable
+    int NF;                                 // number of features (genes, taxa, etc)
+    int NB_f;                               // number of feature factor levels
+    int NFB;                                // number of feature factors
+    array[NB_f] int idx_f;                  // mapping of sigmas to feature factor levels including residuals
+    matrix[NB_f,NF] X_f;                    // model matrix for features (e.g. taxonomy, functional annotations, gene length, etc.). must include intercept but not residuals
+    matrix[NB_f-1,NB_f-1] X_fR_inv;         // inverse(qr_thin_R(X_f[2:,'))', precomputed in R to use pivoting and be more stable
+    array[NF,NS] int count;                 // observations
+    real prior_scale_p;
+    real prior_scale_a;
+    int K_s;
+}
+transformed data {
+    array[NB_f-1] int idx_f2;
+    array[NB_s+K_s] int idxk_s;
+    array[NF*NS] int count_1d = to_array_1d(count);
+    int N_zero = num_zeros(count_1d);
+    array[N_zero] int i0;
+    array[NF*NS-N_zero] int in0;
+    int i0i = 1;
+    int in0i = 1;
+    for(f in 2:NB_f) idx_f2[f-1] = idx_f[f] - 1;
+    idxk_s[1:NB_s] = idx_s;
+    for(s in 1:K_s) idxk_s[NB_s+s] = NSB + 1;
+    for(c in 1:size(count_1d)) {
+        if(count_1d[c] == 0) {
+            i0[i0i] = c;
+            i0i += 1;
+        } else {
+            in0[in0i] = c;
+            in0i += 1;
+        }
+    }
+}
+parameters {
+    real<lower=0>                      global_scale_prevalence;
+    real<lower=0>                      global_scale_abundance;
+    vector<lower=0>[(NSB+2)*(NFB+1)-2] sd_prevalence_norm;
+    vector<lower=0>[(NSB+2)*NFB-1]     sd_abundance_norm;
+    matrix[NB_s+K_s,NB_f]              beta_prevalence_i_tilde;
+    matrix[NB_s+K_s,NF]                beta_prevalence_s_tilde;
+    matrix[NS,NB_f]                    beta_prevalence_f_tilde;
+    matrix[NB_s+K_s,NB_f-1]            beta_abundance_i_tilde;
+    matrix[NB_s+K_s,NF]                beta_abundance_s_tilde;
+    matrix[NS,NB_f-1]                  beta_abundance_f_tilde;
+    matrix[NS,NF]                      residuals;
+    vector[NS]                         multinomial_nuisance;
+    cholesky_factor_cov[NS,K_s]        L_s;
+}
+transformed parameters {
+    matrix<lower=0>[NSB+2,NFB+1] sd_prevalence = to_matrix(append_row(100, append_row(sd_prevalence_norm, 1.0)), NSB+2, NFB+1) * (prior_scale_p * global_scale_prevalence);
+    matrix<lower=0>[NSB+2,NFB]   sd_abundance  = to_matrix(append_row(sd_abundance_norm, 1.0),                   NSB+2, NFB)   * (prior_scale_a * global_scale_abundance);
+    matrix[NB_s+K_s,NB_f]        beta_prevalence_i;
+    matrix[NB_s+K_s,NF]          beta_prevalence_s;
+    matrix[NS,NB_f]              beta_prevalence_f;
+    matrix[NB_s+K_s,NB_f-1]      beta_abundance_i;
+    matrix[NB_s+K_s,NF]          beta_abundance_s;
+    matrix[NS,NB_f-1]            beta_abundance_f;
+    beta_prevalence_i[1,]         = beta_prevalence_i_tilde[1,];
+    beta_prevalence_i[,1]         = beta_prevalence_i_tilde[,1];
+    beta_prevalence_i[(NB_s+1):,] = beta_prevalence_i_tilde[(NB_s+1):,];
+    beta_prevalence_i[2:NB_s,2:]  = X_sR_inv * beta_prevalence_i_tilde[2:NB_s,2:] * X_fR_inv;
+    beta_prevalence_s[1,]         = beta_prevalence_s_tilde[1,];
+    beta_prevalence_s[(NB_s+1):,] = beta_prevalence_s_tilde[(NB_s+1):,];
+    beta_prevalence_s[2:NB_s,]    = X_sR_inv * beta_prevalence_s_tilde[2:NB_s,];
+    beta_prevalence_f[,1]         = beta_prevalence_f_tilde[,1];
+    beta_prevalence_f[,2:]        = beta_prevalence_f_tilde[,2:] * X_fR_inv;
+    beta_abundance_i[1,]          = beta_abundance_i_tilde[1,];
+    beta_abundance_i[(NB_s+1):,]  = beta_abundance_i_tilde[(NB_s+1):,];
+    beta_abundance_i[2:NB_s,]     = X_sR_inv * beta_abundance_i_tilde[2:NB_s,] * X_fR_inv;
+    beta_abundance_s[1,]          = beta_abundance_s_tilde[1,];
+    beta_abundance_s[(NB_s+1):,]  = beta_abundance_s_tilde[(NB_s+1):,];
+    beta_abundance_s[2:NB_s,]     = X_sR_inv * beta_abundance_s_tilde[2:NB_s,];
+    beta_abundance_f[,2:]         = beta_abundance_f_tilde * X_fR_inv;
+}
+model {
+    matrix[NS,NB_s+K_s] XL_s = append_col(X_s,L_s);
+    matrix[NS,NF] prevalence
+        =    XL_s * (beta_prevalence_s .* sd_prevalence[idxk_s, rep_array(NFB+1,NF)])
+          + (XL_s * (beta_prevalence_i .* sd_prevalence[idxk_s, idx_f])
+             +      (beta_prevalence_f .* sd_prevalence[rep_array(NSB+2,NS), idx_f])) * X_f;
+    matrix[NS,NF] abundance
+        =    XL_s * (beta_abundance_s  .* sd_abundance[idxk_s, rep_array(NFB,NF)])
+          + (XL_s * (beta_abundance_i  .* sd_abundance[idxk_s, idx_f2])
+             +      (beta_abundance_f  .* sd_abundance[rep_array(NSB+2,NS), idx_f2])) * X_f[2:,]
+          + residuals                   * sd_abundance[NSB+2, NFB]
+          + rep_matrix(multinomial_nuisance, NF);
+    // priors
+    target += std_normal_lpdf(global_scale_prevalence);
+    target += std_normal_lpdf(global_scale_abundance);
+    target += std_normal_lpdf(sd_prevalence_norm);
+    target += std_normal_lpdf(sd_abundance_norm);
+    target += std_normal_lpdf(to_vector(beta_prevalence_i));
+    target += std_normal_lpdf(to_vector(beta_prevalence_s));
+    target += std_normal_lpdf(to_vector(beta_prevalence_f));
+    target += std_normal_lpdf(to_vector(beta_abundance_i));
+    target += std_normal_lpdf(to_vector(beta_abundance_s));
+    target += std_normal_lpdf(to_vector(beta_abundance_f));
+    target += std_normal_lpdf(to_vector(residuals));
+    for(k in 1:K_s) target += std_normal_lpdf(L_s[k:,k]);
+    // likelihood
+    target += zip_lpmf(count_1d | to_vector(prevalence), to_vector(abundance), i0, in0);
+}
---
+++ microbiomes/zip_glm/zip_glm_run_qr.r
@@ -0,0 +1,212 @@
+print(sessionInfo())
+cat(paste('DESeq2 version:', packageVersion('DESeq2'), '\n'))
+cat(paste('cmdstanr version:', packageVersion('cmdstanr'), '\n'))
+cat(paste('cmdstan version:', cmdstanr::cmdstan_version(), '\n'))
+
+## get user-edited environmental variables outdir, counts_fp, etc
+newargs <- commandArgs(TRUE)
+
+load(newargs[[1]])
+taxid_fp <- newargs[[2]]
+samplenames <- read.table(newargs[[3]],sep='\t',header=T)
+metadat <- read.table(newargs[[4]],sep='\t',header=T)
+id_conversion <- read.table(newargs[[5]],sep='\t',header=T)
+
+outdir <- newargs[[6]]
+K_s <- as.numeric(newargs[[7]])
+nchains <- as.numeric(newargs[[8]])
+nthreads <- as.numeric(newargs[[9]])
+opencl <- as.logical(newargs[[10]])
+opencl_device <- as.numeric(newargs[[11]])
+model_dir <- newargs[[12]]
+algorithm <- newargs[[13]]
+##
+
+rownames(seqtab) <- sapply(rownames(seqtab), function(x) samplenames$Sample[samplenames$Tag == sub('.fastq.gz','',sub('-',':',x))])
+
+seqtab_F <- seqtab[grep('UFL',rownames(seqtab)),]
+
+seqtab_M <- t(sapply(unique(sub('_.*','',rownames(seqtab_F))), function(x) apply(seqtab_F[grep(x,rownames(seqtab_F)),], 2, sum)))
+
+metadat$id_argaly <- id_conversion$id_argaly[match(metadat$ID..Alison, id_conversion$id_alison)]
+
+m2 <- metadat[match(rownames(seqtab_M),metadat$id_argaly),]
+
+taxid <- read.table(taxid_fp,sep='\t',row.names = 1, header=TRUE)
+
+## set up output directory
+dir.create(file.path(outdir, 'zip_glm'))
+##
+
+counts <- t(seqtab_M)
+counts <- counts[apply(counts,1,sd) > 0,]
+counts_rlog <- DESeq2::rlog(counts)
+sampleOrder <- order(apply(abs(cov2cor(crossprod(t(apply(counts_rlog,1,function(x) x-mean(x)))))),2,mean), decreasing=TRUE)
+featureOrder <- order(apply(abs(cov2cor(tcrossprod(apply(counts_rlog,2,function(x) x-mean(x))))),2,mean), decreasing=TRUE)
+counts <- counts[featureOrder,sampleOrder]
+
+relabund <- apply(counts, 2, function(x) x / sum(x))
+
+countsbin <- t(as.matrix(counts))
+countsbin[countsbin > 0] <- 1
+
+countsmod <- t(as.matrix(counts))
+countsmod[countsmod==0] <- 0.5
+logcountsmod <- log(countsmod)
+
+rownames(m2) <- m2$id_argaly
+m2 <- m2[colnames(counts)[colnames(counts) %in% rownames(m2)],]
+m2$Location <- as.factor(m2$Location)
+
+NS    = ncol(counts)
+NF    = nrow(counts)
+
+X_s <- cbind(Intercept=1, model.matrix(~ 0 + Location, m2)) ## standardize continuous variables before placing in model
+X_s[,-1] <- apply(X_s[,-1], 2, function(x) x - mean(x))
+rownames(X_s) <- rownames(m2)
+X_s <- X_s[colnames(counts),]
+
+idx_s <- c(1, rep(2, nlevels(m2$Location)))
+NSB <- max(idx_s)
+NB_s <- length(idx_s)
+
+## work on this section to make sure columns are unique and so that taxa are filtered out if they apply to ALL samples as well as none
+hi <- cbind('Intercept',unique(taxid))
+
+estimables <- lapply(2:(ncol(hi)-1), function(x) {
+  y <- unique(hi[,1:x])
+  keepers <- apply(y, 1, function(z) sum(apply(y[,1:(x-1),drop=F],1, function(r) identical(z[1:(x-1)],r))) > 1 & !is.na(z[[x]]))
+  return(unname(y[keepers,x]))
+})
+
+X_f <- cbind(Intercept=1, do.call(cbind, sapply(1:length(estimables), function(x) sapply(estimables[[x]], function(y) as.numeric(taxid[,x] == y)))))
+rownames(X_f) <- rownames(taxid)
+X_f <- X_f[rownames(counts),]
+X_f[is.na(X_f)] <- 0
+mode(X_f) <- 'numeric'
+X_f_nonUnique <- colSums(X_f)>1 & !duplicated(t(X_f))
+X_f <- X_f[,X_f_nonUnique]
+X_f[,-1] <- apply(X_f[,-1], 2, function(x) x-mean(x))
+##
+
+idx_f = c(1, rep(2,ncol(X_f)-1))
+NFB   = max(idx_f)
+NB_f  = length(idx_f)
+
+prior_scale_p <- sqrt(exp(mean(log(apply(countsbin,2,var)[apply(countsbin,2,var) > 0]))))
+prior_scale_a <- sqrt(exp(mean(log(apply(counts,2,function(x) var(log(x[x>0])))))))
+
+qr.xs <- qr(X_s[,2:])
+rank_X_s <- qr.xs$rank
+R_s <- qr.R(qr.xs)
+R_inv_s <- MASS::ginv(R_s)[,1:rank_X_s]
+
+I_diff_AA_reduced_s <- diag(1,nrow(R_inv_s)) - R_inv_s %*% R_s[1:rank_X_s,]
+qr.is <- qr(I_diff_AA_reduced_s)
+top_rows <- sort(order(abs(diag(qr.R(qr.is))),decreasing=T)[1:(NB_s-rank_X_s)])
+Q_I_s <- qr.Q(qr.is)[,top_rows]
+
+X_sR_inv <- cbind(R_inv_s,Q_I_s)
+
+
+qr.xf <- qr(X_f[,2:])
+rank_X_f <- qr.xf$rank
+R_f <- qr.R(qr.xf)
+R_inv_f <- MASS::ginv(R_f)[,1:rank_X_f]
+
+I_diff_AA_reduced_f <- diag(1,nrow(R_inv_f)) - R_inv_f %*% R_f[1:rank_X_f,]
+qr.if <- qr(I_diff_AA_reduced_f)
+top_rows <- sort(order(abs(diag(qr.R(qr.if))),decreasing=T)[1:(NB_f-rank_X_f)])
+Q_I_s <- qr.Q(qr.is)[,top_rows]
+
+X_sR_inv <- cbind(R_inv_f,Q_I_f)
+
+
+standat <- list(NS            = NS,
+                NB_s          = NB_s,
+                NSB           = NSB,
+                idx_s         = idx_s,
+                X_s           = X_s,
+                X_sR_inv      = X_sR_inv,
+                NF            = NF,
+                NB_f          = NB_f,
+                NFB           = NFB,
+                idx_f         = idx_f,
+                X_f           = t(X_f),
+                X_fR_inv      = t(X_fR_inv),
+                count         = counts,
+                prior_scale_a = prior_scale_a,
+                prior_scale_p = prior_scale_p,
+                K_s           = K_s)
+
+inits <- list(global_scale_prevalence = 0.1,
+              global_scale_abundance  = 0.1,
+              sd_prevalence_norm      = rep(0.1, (NSB+2)*(NFB+1)-2),
+              sd_abundance_norm       = rep(0.1, (NSB+2)*NFB-1),
+              beta_prevalence_i       = matrix(0,NB_s+K_s,NB_f),
+              beta_prevalence_s       = matrix(0,NB_s+K_s,NF),
+              beta_prevalence_f       = matrix(0,NS,NB_f),
+              beta_abundance_i        = matrix(0,NB_s+K_s,NB_f-1),
+              beta_abundance_s        = matrix(0,NB_s+K_s,NF),
+              beta_abundance_f        = matrix(0,NS,NB_f-1),
+              residuals               = matrix(0,NS,NF),
+              multinomial_nuisance    = apply(counts,2,function(x) log(mean(x))),
+              L_s                     = diag(1,NS,K_s))
+
+save.image(file.path(outdir, 'zip_glm', 'zip_glm_setup.RData'))
+
+cmdstanr::write_stan_json(standat, file.path(outdir, 'zip_glm', 'zip_test_data.json'))
+cmdstanr::write_stan_json(inits, file.path(outdir, 'zip_glm', 'zip_test_inits.json'))
+
+setwd(cmdstanr::cmdstan_path())
+system(paste0(c('make ', 'make STAN_OPENCL=true ')[opencl+1], file.path(model_dir,'zip_glm_qr')))
+
+sampling_commands <- list(hmc = paste('./zip_glm_qr',
+                                      paste0('data file=',path.expand(file.path(outdir, 'zip_glm', 'zip_test_data.json'))),
+                                      paste0('init=',path.expand(file.path(outdir, 'zip_glm', 'zip_test_inits.json'))),
+                                      'output',
+                                      paste0('file=',path.expand(file.path(outdir, 'zip_glm', 'zip_test_data_samples.csv'))),
+                                      paste0('refresh=', 1),
+                                      'method=sample',
+                                      paste0('num_chains=',nchains),
+                                      'algorithm=hmc',
+                                      'stepsize=0.1',
+                                      'engine=nuts',
+                                      'max_depth=12',
+                                      'adapt t0=10',
+                                      'delta=0.8',
+                                      'kappa=0.75',
+                                      'num_warmup=1000',
+                                      'num_samples=1000',
+                                      paste0('num_threads=',nthreads),
+                                      (paste0('opencl platform=0 device=', opencl_device))[opencl],
+                                      sep=' '),
+                          advi = paste('./zip_glm',
+                                       paste0('data file=',path.expand(file.path(outdir, 'zip_glm', 'zip_test_data.json'))),
+                                       'init=0',
+                                       'output',
+                                       paste0('file=',path.expand(file.path(outdir, 'zip_glm', 'zip_test_data_samples.csv'))),
+                                       paste0('refresh=', 100),
+                                       'method=variational algorithm=meanfield',
+                                       #'grad_samples=1',
+                                       #'elbo_samples=1',
+                                       'iter=20000',
+                                       'eta=0.1',
+                                       'adapt engaged=0',
+                                       'tol_rel_obj=0.01',
+                                       #'eval_elbo=1',
+                                       'output_samples=1000',
+                                       (paste0('opencl platform=0 device=', opencl_device))[opencl],
+                                       sep=' '))
+
+setwd(model_dir)
+print(sampling_commands[[algorithm]])
+print(date())
+system(sampling_commands[[algorithm]])
+
+#stan.fit.var <- cmdstanr::read_cmdstan_csv(Sys.glob(path.expand(file.path(outdir,'zip_glm','zip_test_data_samples_*.csv'))),
+#                                           format = 'draws_array')
+
+#summary(stan.fit.var$post_warmup_sampler_diagnostics)
+#plot(apply(stan.fit.var$post_warmup_draws[,1,paste0('L_s[',1:NS,',1]')], 3, mean), apply(stan.fit.var$post_warmup_draws[,1,paste0('L_s[',1:NS,',2]')],3,mean), xlab = "PCA1", ylab = "PCA2",axes = TRUE, main = "First samplewise latent variables", col=as.factor(m2$env.features), pch=16)
+#taxid[do.call(order, as.data.frame(taxid)),]
