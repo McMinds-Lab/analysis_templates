63ec84e88c264d9178869915670e7178413498db
Ryan McMinds <r.mcminds@gmail.com>
2022-06-14 12:51:44 -0400

    Update 02_DE-kupl-run.sh

--- generic_dekupl/02_DE-kupl-run.sh
+++ generic_dekupl/02_DE-kupl-run.sh
@@ -1,18 +1,29 @@
+## three positional arguments specifying 1) the directory containing fastqs, 2) the output analysis directory, and 3) the file path of the config file
+indir=$1
+outdir=$2
+config=$3
+
+mkdir -p ${outdir}/02_DE-kupl-run/logs
+cp ${config} ${outdir}/02_DE-kupl-run/config.json
+
+cat <<EOF > ${outdir}/02_DE-kupl-run/02_DE-kupl-run.sbatch
 #!/bin/bash
 #SBATCH --qos=rra
 #SBATCH --partition=rra
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=187G
-#SBATCH --job-name=zeroinfl
-#SBATCH --output=file.path
+#SBATCH --job-name=02_DE-kupl-run
+#SBATCH --output=${outdir}/02_DE-kupl-run/logs/02_DE-kupl-run_%a.log
 #SBATCH --cpus-per-task=24
-
-outdir="file.path"
-
-mkdir -p ${outdir}/logs
 
 module purge
 module load hub.apps/anaconda3
 source activate dekupl
 
-dekupl-run --configfile file.path/config.json -j24 --resources ram=187000 -p
+dekupl-run --configfile ${outdir}/02_DE-kupl-run/config.json -j\${SLURM_CPUS_PER_TASK} --resources ram=\${SLURM_MEM_PER_NODE} -p
+
+EOF
+
+if $autorun; then
+    sbatch ${outdir}/02_DE-kupl-run/02_DE-kupl-run.sbatch
+fi
