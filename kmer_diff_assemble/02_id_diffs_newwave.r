429cffbe9e13add32fe5589c9c2c9abd009ab07e
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2022-08-01 11:33:42 -0400

    bugfix

--- kmer_diff_assemble/02_id_diffs_newwave.r
+++ kmer_diff_assemble/02_id_diffs_newwave.r
@@ -11,12 +11,13 @@ outdir <- args[[6]]
 input_children <- strsplit(args[[7]], ' ')[[1]]
 
 if(any(is.na(suppressWarnings(as.numeric(input_children))))) {
+  ## assuming exclusive use of each node
   cat('interpreting "children" option to be node names, not number of processes\n')
-  firstnodesize <- as.numeric(system(paste0('sinfo --Node --nodes ', input_children[[1]],' --format %c | tail -1'), internal=TRUE))
+  firstnodesize <- as.numeric(system(paste0('sinfo --Node --nodes ', input_children[[1]],' --format %c | tail -1'), intern=TRUE))
   children <- rep(input_children[[1]], ceiling(firstnodesize / 2))
   if(length(input_children) > 1) {
     children <- c(children, as.vector(sapply(input_children[-1], \(x) {
-      nodesize <- as.numeric(system(paste0('sinfo --Node --nodes ', x,' --format %c | tail -1'), internal=TRUE))
+      nodesize <- as.numeric(system(paste0('sinfo --Node --nodes ', x,' --format %c | tail -1'), intern=TRUE))
       rep(x, max(1, nodesize - 1))
     })))
   }
