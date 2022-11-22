70e51f4330b3c8343232a1857f3130f2f0a2b829
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2022-11-22 13:08:15 -0500

    bcl_convert

---
+++ getting_data/bcl_convert_all.sh
@@ -0,0 +1,28 @@
+## two positional arguments specifying 1) an input basecall directory and 2) the output directory
+
+indir=$1
+outdir=$2
+
+mkdir -p ${outdir}
+
+cat <<EOF > ${outdir}/bcl_convert_all.sbatch
+#!/bin/bash
+#SBATCH --qos=rra
+#SBATCH --partition=rra
+#SBATCH --time=6-00:00:00
+#SBATCH --mem=${maxram}
+#SBATCH --job-name=bcl_convert_all
+#SBATCH --output=${outdir}/bcl_convert_all.log
+
+/shares/omicshub/apps/bcl-convert/bcl-convert --version
+
+/shares/omicshub/apps/bcl-convert/bcl-convert --no-sample-sheet True --bcl-input-directory ${indir} --output-directory ${outdir}
+
+EOF
+
+if $autorun; then
+
+sbatch ${outdir}/bcl_convert_all.sbatch
+
+fi
+
