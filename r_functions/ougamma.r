242a4b3aa5d2e3e479a37abbd0600fb1eea81308
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2022-11-22 11:33:47 -0500

    download basespace

--- r_functions/ougamma.r
+++ r_functions/ougamma.r
@@ -8,15 +8,15 @@ rougamma <- function(n,shape,scale,time,sigma2r_mean,start) {
   ## sigma2r_mean = the mean squared rate of evolution /relative to the stationary variance/
   ## start = the starting value from which the distribution evolves
 
-  ## integration of sigma^2 over a timeperiod can be modeled as the sum of many small deviations, of a number proportional to time, each drawn from a gamma distribution
+  ## integration of sigma^2 over a time period can be modeled as the sum of many small deviations, of a number proportional to time, each drawn from a gamma distribution
   ## the sum of gamma distributions is another gamma with shape equal to the sum of the shapes of the input
   ## a sum of N deviations each with shape S and expectation E has distribution gamma(N*S,S/E), which can be generalized to a continuous integral with real-valued N (such as time)
 
     sigma2r_t <- rgamma(n, time*shape, shape/sigma2r_mean)
 
-  ## note that in this model, the expectation for total evolution (sigma2r_t) increases proportionally with time, but so does the precision of our estimate
+  ## note that in this model, the expectation for total evolution (sigma2r_t) increases proportionally with time, but so does the relative precision of our estimate
   ## small time periods have more variable and more skewed possibilities, with more 'outliers'
-  ## as the base shape goes to infinity, variability of evolutionary rate goes to 0, and the model converges to standard OU evolution with fixed sigma
+  ## as the base shape goes to infinity, variability of evolutionary rate goes to 0, and the model converges to standard OU evolution with sigma2r_t fixed to sigma2r_mean * t
 
   ## stationary variance = sigma^2/(2*theta)
   ## if stationary variance is fixed to x, then theta = sigma^2/(2x)
@@ -25,7 +25,7 @@ rougamma <- function(n,shape,scale,time,sigma2r_mean,start) {
 
     theta_t <- 0.5 * sigma2r_t
 
-  ## the deterministic portion of OU evolution (when the attractor is 0) can be calculated as a simple fraction of the starting value, with the fraction defined as:
+  ## the deterministic portion of OU evolution (when the attractor is 0, such that we are modeling deviances from the stationary mean) can be calculated as a simple fraction of the starting value, with the fraction defined as:
 
     oup <- exp(-theta_t)
 
@@ -41,5 +41,7 @@ rougamma <- function(n,shape,scale,time,sigma2r_mean,start) {
 
     return(rnorm(n,mu,dev))
 
-}
+  ## this should be trivially extendable to multivariate OU if it is assumed that all variables' rates of evolution (relative to their stationary variance, and thus thetas) are the same
+  ## more work is needed to allow different variables to evolve at different relative rates
 
+}
