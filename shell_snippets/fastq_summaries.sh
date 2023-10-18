0b14ce5a33c9aa48d19f51d3a3d3c4c2cc259727
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2023-10-18 15:31:49 -0400

    amplicon readme

--- shell_snippets/fastq_summaries.sh
+++ shell_snippets/fastq_summaries.sh
@@ -8,13 +8,16 @@ zcat file.fastq.gz | grep '^@' --no-group-separator -A 1 | awk '!(NR%2) { currle
 
 ## filter fasta to only keep records whose first header field matches names in a list
 ## NR == FNR means execute only when the total record number is same as the per-file record number (e.g. only do these commands that store names in an array for the first file)
-## RS='(^|\n)>' says to process the fasta recognizing that records are separated by a newline plus carat. Within each record, fields will be separated by whitespace including spaces and other newlines, but the first field should always be the name of the sequence
+## RS='\n>' says to process the fasta recognizing that records are separated by a newline plus carat. Within each record, fields will be separated by whitespace including spaces and other newlines, but the first field should always be the name of the sequence
 ## sub and print ">" take care of formatting the newlines and carats in the output
 awk 'NR == FNR {
        a[$1]++
        next
      }
+     NR > FNR && FNR == 1 {
+       sub(/^>/, "")
+     }
      $1 in a {
        sub(/\n$/, "")
        print ">"$0
-     }' seqlist.txt RS='(^|\n)>' file.fasta > file_filt.fasta
+     }' seqlist.txt RS='\n>' file.fasta > file_filt.fasta
