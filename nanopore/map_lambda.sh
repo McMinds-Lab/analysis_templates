ba75d22eb91d1e3744d691692aa4d483cc603403
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2023-12-12 12:34:39 -0500

    map using dorado output

--- nanopore/map_lambda.sh
+++ nanopore/map_lambda.sh
@@ -8,24 +8,25 @@ unzip /Users/Ryan/outputs/lambda/lambda_genome.zip -d /Users/Ryan/outputs/lambda
 rm /Users/Ryan/outputs/lambda/lambda_genome.zip
 
 ## map reads to lambda reference
+conda activate samtools
+samtools fastq /Users/Ryan/outputs/nano2/duplex.bam | gzip --best > /Users/Ryan/outputs/nano2/duplex.fastq.gz
 conda activate minimap2
-mkdir /Users/Ryan/outputs/sym1
-minimap2 -ax map-ont /Users/Ryan/outputs/lambda/lambda_genome/ncbi_dataset/data/genomic.fna /Users/Ryan/outputs/nano/*/*.fastq.gz > /Users/Ryan/outputs/sym1/lambdamap.sam
+minimap2 -ax map-ont /Users/Ryan/outputs/lambda/lambda_genome/ncbi_dataset/data/genomic.fna /Users/Ryan/outputs/nano2/duplex.fastq.gz > /Users/Ryan/outputs/nano2/lambdamap.sam
 
 ## find unmapped reads
 conda activate samtools
-samtools view -S -f4 /Users/Ryan/outputs/sym1/lambdamap.sam | cut -f1 | sort | uniq > /Users/Ryan/outputs/sym1/notlambda_reads.txt
+samtools view -S -f4 /Users/Ryan/outputs/nano2/lambdamap.sam | cut -f1 | sort | uniq > /Users/Ryan/outputs/nano2/notlambda_reads.txt
 
 conda activate seqtk
-seqtk subseq <(zcat </Users/Ryan/outputs/nano/*/*.fastq.gz) /Users/Ryan/outputs/sym1/notlambda_reads.txt > /Users/Ryan/outputs/sym1/notlambda_reads.fastq
+seqtk subseq <(zcat </Users/Ryan/outputs/nano2/duplex.fastq.gz) /Users/Ryan/outputs/nano2/notlambda_reads.txt > /Users/Ryan/outputs/nano2/notlambda_reads.fastq
 
 ## assemble and or annotate remainder
 conda activate flye
-flye -m 1000 --meta --nano-raw /Users/Ryan/outputs/sym1/notlambda_reads.fastq -o /Users/Ryan/outputs/sym1/flye_out
+flye -m 1000 --meta --nano-raw /Users/Ryan/outputs/nano2/notlambda_reads.fastq -o /Users/Ryan/outputs/nano2/flye_out
 
 ## annotate
 conda activate mmseqs2
-mmseqs databases UniProtKB/Swiss-Prot /Users/Ryan/outputs/sym1/swissprot /Users/Ryan/outputs/sym1/tmp
-mmseqs easy-taxonomy /Users/Ryan/outputs/sym1/flye_out/assembly.fasta /Users/Ryan/outputs/sym1/swissprot /Users/Ryan/outputs/sym1/mmseqtax /Users/Ryan/outputs/sym1/tmp
+mmseqs databases --compressed 1 GTDB /Users/Ryan/outputs/nano2/GTDB /Users/Ryan/outputs/nano2/tmp
+mmseqs easy-taxonomy --compressed 1 /Users/Ryan/outputs/nano2/flye_out/assembly.fasta /Users/Ryan/outputs/nano2/GTDB /Users/Ryan/outputs/nano2/mmseqtax /Users/Ryan/outputs/nano/tmp
 
 ## consider aligning to this and then looking at remainder: https://www.ncbi.nlm.nih.gov/nuccore/HG966617.1
