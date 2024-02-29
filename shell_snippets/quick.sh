4452031b39984e367c0620fcce7f9270013a7e9f
Ryan McMinds <r.mcminds@gmail.com>
2024-02-28 19:03:39 -0500

    pipe thoughts

--- shell_snippets/quick.sh
+++ shell_snippets/quick.sh
@@ -1,2 +1,6 @@
 # reverse complement a nucleotide sequence with IAUPAC degeneracy codes
 echo ${seq} | tr ACGTRYSWKMBVDHacgtryswkmbvdh TGCAYRSWMKVBHDtgcayrswmkvbhd | rev
+
+# example of piping between subshells that need different conda environments
+# what makes the pipe 'skip' the second 'conda activate'? 
+(conda activate samtools; samtools help) | (conda activate minimap2; cat)
