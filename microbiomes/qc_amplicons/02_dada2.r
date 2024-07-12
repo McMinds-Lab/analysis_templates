77ee9af36fcad7fd98ecae6b2f9bb7a3cdb42f24
Ryan McMinds <r.mcminds@gmail.com>
2024-07-12 15:18:31 -0400

    robustify dada

--- microbiomes/qc_amplicons/02_dada2.r
+++ microbiomes/qc_amplicons/02_dada2.r
@@ -14,20 +14,23 @@ filt_reads <- file.path(outdir, "filtered", basename(merged_reads))
 # filter out reads that are probably just primer dimer
 filt <- dada2::filterAndTrim(merged_reads, filt_reads, truncQ = 0, minLen = 50, compress = TRUE, multithread = nthreads, qualityType='FastqQuality')
 
+filt_reads_exist <- filt_reads[file.exists(filt_reads)]
+stopifnot(length(filt_reads_exist) > 0)
+
 # note that the q-scores appear to be inflated relative to Nanoplot results. This is because dada2 takes mean of raw q-scores, while nanoplot takes mean of probabilities (e.g. arithmetic vs geometric means)
 png(file.path(outdir, "quality_profile.png"), height=600, width=600)
-dada2::plotQualityProfile(filt_reads[1:10])
+dada2::plotQualityProfile(filt_reads_exist[1:min(10, length(filt_reads_exist))])
 dev.off()
 
 # only reads as many samples as needed to get to 1e8 bases (can be changed), with samples randomized
-err_merged_reads <- dada2::learnErrors(filt_reads, multithread = nthreads, randomize = TRUE, MAX_CONSIST = 30, pool = 'pseudo', qualityType='FastqQuality')
+err_merged_reads <- dada2::learnErrors(filt_reads_exist, multithread = nthreads, randomize = TRUE, MAX_CONSIST = 30, pool = 'pseudo', qualityType='FastqQuality')
 
 pdf(file.path(outdir, "dada_err.pdf"))
 dada2::plotErrors(err_merged_reads, nominalQ=TRUE)
 dev.off()
 
 #denoise
-derepped <- dada2::derepFastq(filt_reads)
+derepped <- dada2::derepFastq(filt_reads_exist)
 dada_merged <- dada2::dada(derepped, err = err_merged_reads, pool = 'pseudo', multithread = nthreads)
 
 #Construct ASV table
